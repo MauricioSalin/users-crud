2959a8e78080a97bdc6f7c414f24b2150e32f1b6
MauricioSalin <mauricio_salin@hotmail.com>
2018-01-27 17:26:11 -0200

    adiciona login no bacno

--- app.js
+++ app.js
@@ -2,6 +2,20 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const exphbs = require('express-handlebars')
+const MongoClient = require('mongodb').MongoClient
+
+const url = ''
+
+MongoClient.connect(url, function(err, db) {
+    db = db.db('usercrud')
+    console.log("Conectado ao banco!")
+
+const collection = db.collection('usuarios')
+
+collection.find({}).toArray(function(err, person) {
+    console.log(JSON.stringify(person, null, 2))
+    });
+});
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
