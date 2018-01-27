f9575c23090bf7f5eb3222942d1f4cf7421c9366
MauricioSalin <mauricio_salin@hotmail.com>
2018-01-27 15:46:41 -0200

    adiciona bulma/handlebars

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-//CRUD USUARIOS
+const express = require('express')
+const app = express()
+const path = require('path')
+const exphbs = require('express-handlebars')
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+app.get('/', function(req, res) {
+    res.render('index')
+});
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
