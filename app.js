3eb8172fbb8415f36ec785830afccfe651554500
MauricioSalin <mauricio_salin@hotmail.com>
2018-01-27 19:59:10 -0200

    adiciona bodyparser

--- app.js
+++ app.js
@@ -3,12 +3,16 @@ const app = express()
 const path = require('path')
 const exphbs = require('express-handlebars')
 const MongoClient = require('mongodb').MongoClient
+var bodyParser = require('body-parser');
 
 const url = ''
 
 MongoClient.connect(url, function(err, db) {
     db = db.db('usercrud')
     console.log("Conectado ao banco!")
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));    
 
 const collection = db.collection('usuarios')
 
@@ -21,13 +25,19 @@ app.get('/', function(req, res) {
             users: Object
         }
         accounts.users = JSON.parse(JSON.stringify(result, null, 2))
-
-        console.log(accounts)
-
-
         res.render('index', accounts);
         });
 });
+
+app.get('/newUser', function(req, res) {
+        res.render('newUser')     
+});
+
+app.post('/newUser', function(req, res) {
+  let nome = req.body.nome
+  let email = req.body.email
+  console.log(nome)
+})
 
 });
 
