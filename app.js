4c6ee04b36c272d12229e8aa52da9e4c75fe15c5
MauricioSalin <mauricio_salin@hotmail.com>
2018-01-28 20:33:32 -0200

    adiciona editar usuario

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const exphbs = require('express-handlebars')
 const MongoClient = require('mongodb').MongoClient
 const bodyParser = require('body-parser');
 
+const ObjectId = require("mongodb").ObjectId;
 const url = ''
 
 MongoClient.connect(url, function(err, db) {
@@ -21,16 +22,17 @@ app.set('view engine', 'handlebars')
 
 app.get('/', function(req, res) {
     collection.find({}).toArray(function(err, result) {
-        let accounts = {
-            users: Object
-        }
+        let accounts = new Object
         accounts.users = JSON.parse(JSON.stringify(result, null, 2))
         res.render('index', accounts);
         });
 });
 
 app.get('/newUser', function(req, res) {
-        res.render('newUser')     
+    let newAccount = new Object
+    newAccount.user = new Object
+    newAccount.action = "/newUser"    
+        res.render('newUser', newAccount)     
 });
 
 app.post('/newUser', function(req, res) {
@@ -46,6 +48,34 @@ app.post('/newUser', function(req, res) {
     res.redirect('/')
     });
 })
+
+app.get('/edit/:id', function(req, res) {
+    collection.find(new ObjectId(req.params.id)).toArray(function(err, result) {
+        let account = new Object
+        account.user = JSON.parse(JSON.stringify(result, null, 2))
+        account.action = "/edit/:id"
+        res.render('newUser', account);
+        });    
+});
+
+app.post('/edit/:id', function(req, res) {  
+    let id = new ObjectId(req.params.id)
+    let nome = req.body.nome
+    let sobrenome = req.body.sobrenome
+    let email = req.body.email
+    let cpf = req.body.cpf
+    let telefone = req.body.telefone
+    let data = req.body.data
+    let status = req.body.status
+
+    let newValue = { _id: id, nome, sobrenome, email, cpf, telefone, data, status}
+
+    collection.save(newValue, function(err, result) {
+        console.log(result)
+        res.redirect('/')
+        });
+     });
+
 
 });
 
